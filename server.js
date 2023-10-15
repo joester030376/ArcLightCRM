a9b30af980cb99e390f21af3efc1a623c86d2f14
joester030376 <firehawkdigitalohio@gmail.com>
2023-10-15 14:49:41 -0400

    Add mysql database change to server.js

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to the database.");
+});
+
 
 app.listen(8080, function(err) {
     if (err) console.log('Could not connect to server');
