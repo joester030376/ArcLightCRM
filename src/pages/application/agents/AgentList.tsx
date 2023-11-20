7bf179b40a8e218bae6a89131294c553bee7e146
firehawkdigitalohio <firehawkdigitalohio@gmail.com>
2023-11-20 15:56:18 -0500

    Added Agent list code

--- src/pages/application/agents/AgentList.tsx
+++ src/pages/application/agents/AgentList.tsx
@@ -1,11 +1,97 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
-type Props = {};
-
-const AgentList = (props: Props) => {
-    return (
-        <div>Agent List Page</div>
-    );
+const onButtonClick = (e: React.ChangeEvent<any> , row: any) => {
+    e.stopPropagation();
+    console.log(row);
 }
 
-export default AgentList;
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 100 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 200,
+    editable: true,
+  },
+  {
+    field: 'email',
+    headerName: 'Email',
+    type: 'string',
+    width: 200,
+    editable: true,
+  },
+  {
+    field: 'phone',
+    headerName: 'Phone',
+    type: 'string',
+    width: 200,
+    editable: true,
+  },
+ 
+    { field: 'actions', headerName: 'Actions', width: 200, renderCell: (params) => {
+        return (
+            <div>
+                <Button
+                    onClick={(e) => onButtonClick(e, params.row)}
+                    sx={{
+                        margin: "1px"
+                    }}                
+                >
+                    <DeleteIcon />
+                </Button>
+                <Button
+                    onClick={(e) => onButtonClick(e, params.row)}
+                
+                >
+                    <EditIcon />
+                </Button>
+            </div>
+          
+        );
+      } }      
+ 
+];
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', email: "JonSnow@gmail.com", phone: "555-444-3333" },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
+export default function DataGridDemo() {
+  return (
+    <Box sx={{ height: "80%", width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
+  );
+}
